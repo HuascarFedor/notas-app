e89302285706cbdafbf7a6b3d700f0d842e08d4c
Huascar <huascarfedor@gmail.com>
2024-09-23 12:13:04 -0400

    second commit

--- src/notas/notas.controller.ts
+++ src/notas/notas.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { NotasService } from './notas.service';
 import { CreateNotaDto } from './dto/create-nota.dto';
+import { Nota } from './nota.entity';
 
 @Controller('notas')
 export class NotasController {
@@ -9,5 +10,20 @@ export class NotasController {
   @Post()
   create(@Body() createNotaDto: CreateNotaDto) {
     return this.notasService.create(createNotaDto);
+  }
+
+  @Get()
+  findAll(): Promise<Nota[]> {
+    return this.notasService.findAll();
+  }
+
+  @Get('title/search')
+  async findByTitle(@Query('title') title: string): Promise<Nota[]> {
+    return this.notasService.findByTitle(title);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Nota> {
+    return this.notasService.findOne(+id);
   }
 }
--- src/notas/notas.service.ts
+++ src/notas/notas.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Nota } from './nota.entity';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { CreateNotaDto } from './dto/create-nota.dto';
 
 @Injectable()
@@ -14,5 +14,21 @@ export class NotasService {
   create(createNoteDto: CreateNotaDto): Promise<Nota> {
     const newNota = this.notasRepository.create(createNoteDto);
     return this.notasRepository.save(newNota);
+  }
+
+  findAll(): Promise<Nota[]> {
+    return this.notasRepository.find();
+  }
+
+  findOne(id: number): Promise<Nota> {
+    return this.notasRepository.findOneBy({ id });
+  }
+
+  async findByTitle(title: string): Promise<Nota[]> {
+    return this.notasRepository.find({
+      where: {
+        title: Like(`%${title}%`),
+      },
+    });
   }
 }
