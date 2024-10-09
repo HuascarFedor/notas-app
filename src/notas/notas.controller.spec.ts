8cb20ac3b992e24c3cc6a869ef3271bab939973d
Huascar <huascarfedor@gmail.com>
2024-10-09 10:36:28 -0400

    second commit

--- src/notas/notas.controller.spec.ts
+++ src/notas/notas.controller.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotasController } from './notas.controller';
+import { NotasService } from './notas.service';
+import { CreateNotaDto } from './dto/create-nota.dto';
+
+const mockNota = { id: 1, title: 'Test Note', content: 'Test Content' };
 
 describe('NotasController', () => {
   let controller: NotasController;
+  let service: NotasService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotasController],
+      providers: [
+        {
+          provide: NotasService,
+          useValue: {
+            create: jest.fn().mockResolvedValue(mockNota),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<NotasController>(NotasController);
+    service = module.get<NotasService>(NotasService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a note', async () => {
+    const createNotaDto: CreateNotaDto = {
+      title: 'Test Note',
+      content: 'Test Content',
+    };
+
+    const result = await controller.create(createNotaDto);
+
+    expect(result).toEqual(mockNota);
+
+    expect(service.create).toHaveBeenCalledWith(createNotaDto);
   });
 });
--- src/notas/notas.service.spec.ts
+++ src/notas/notas.service.spec.ts
@@ -1,18 +1,94 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotasService } from './notas.service';
+import { Repository } from 'typeorm';
+import { Nota } from './nota.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
+
+const mockNotaRepository = () => ({
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findOneBy: jest.fn(),
+});
+
+const mockNote = { id: 1, title: 'Test Note', content: 'Test Content' };
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('NotasService', () => {
   let service: NotasService;
+  let repository: MockRepository<Nota>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotasService],
+      providers: [
+        NotasService,
+        {
+          provide: getRepositoryToken(Nota),
+          useValue: mockNotaRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<NotasService>(NotasService);
+    repository = module.get<MockRepository<Nota>>(getRepositoryToken(Nota));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should create a note', async () => {
+    jest.spyOn(repository, 'save').mockResolvedValue(mockNote as Nota);
+
+    // trabaja el metodo del servicio
+    const result = await service.create({
+      title: 'Test Note',
+      content: 'Test Content',
+    });
+
+    expect(result).toEqual(mockNote);
+
+    expect(repository.save).toHaveBeenCalled();
+    expect(repository.create).toHaveBeenCalled();
+  });
+
+  it('should retrieve all notes', async () => {
+    const mockNotes = [
+      { id: 1, title: 'Test Note 1', content: 'Test Content 1' },
+      { id: 2, title: 'Test Note 2', content: 'Test Content 2' },
+    ];
+
+    jest.spyOn(repository, 'find').mockResolvedValue(mockNotes as Nota[]);
+
+    // trabaja el metodo del servicio
+    const result = await service.findAll();
+
+    expect(result).toEqual(mockNotes);
+
+    expect(repository.find).toHaveBeenCalled();
+  });
+
+  it('should retrieve a note by id', async () => {
+    jest.spyOn(repository, 'findOneBy').mockResolvedValue(mockNote as Nota);
+
+    // trabaja el metodo del servicio
+    const result = await service.findOne(1);
+
+    expect(result).toEqual(mockNote);
+
+    expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+  });
+
+  it('should throw a NotFoundException id the note does not exist', async () => {
+    jest.spyOn(repository, 'findOneBy').mockResolvedValue(null);
+
+    // trabaja el metodo del servicio
+    try {
+      await service.findOne(1);
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+      expect(error.message).toBe('La nota con id 1 no existe');
+      expect(error.getStatus()).toBe(404);
+    }
+
+    expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
   });
 });
