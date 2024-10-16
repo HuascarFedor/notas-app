e8f20781964e1ade2a9bc3bf9945e61b19f579b3
Huascar <huascarfedor@gmail.com>
2024-10-16 11:53:11 -0400

    added notas integration service complete

--- src/notas/notas.integration.spec.ts
+++ src/notas/notas.integration.spec.ts
@@ -3,6 +3,7 @@ import { NotasService } from './notas.service';
 import { Nota } from './nota.entity';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
 
 describe('Notas Integration (NotasService)', () => {
   let service: NotasService;
@@ -55,5 +56,112 @@ describe('Notas Integration (NotasService)', () => {
     expect(notasEnBaseDatos).toBeDefined();
     expect(notasEnBaseDatos.title).toEqual(nuevaNota.title);
     expect(notasEnBaseDatos.content).toEqual(nuevaNota.content);
+  });
+
+  it('Deberia obtener todas las notas de la base de datos', async () => {
+    await repository.query('DELETE FROM nota;');
+
+    await repository.save([
+      { title: 'Nota 1', content: 'Contenido 1' },
+      { title: 'Nota 2', content: 'Contenido 2' },
+    ]);
+
+    const notas = await service.findAll();
+    expect(notas.length).toBe(2);
+    expect(notas[0].title).toBe('Nota 1');
+    expect(notas[1].title).toBe('Nota 2');
+  });
+
+  it('Debería obtener una nota por ID', async () => {
+    await repository.query('DELETE FROM nota;');
+
+    const nuevaNota = await repository.save({
+      title: 'Nota específica',
+      content: 'Contenido específico',
+    });
+
+    const notaEncontrada = await service.findOne(nuevaNota.id);
+
+    expect(notaEncontrada).toBeDefined();
+    expect(notaEncontrada.title).toEqual('Nota específica');
+    expect(notaEncontrada.content).toEqual('Contenido específico');
+  });
+
+  it('Debería lanzar NotFoundException al no encontrar una nota por ID', async () => {
+    const notaInexistente = 999;
+    try {
+      await service.findOne(notaInexistente);
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+      expect(error.message).toBe(`La nota con id ${notaInexistente} no existe`);
+    }
+  });
+
+  it('Debería actualizar una nota existente', async () => {
+    await repository.query('DELETE FROM nota;');
+
+    const nuevaNota = await repository.save({
+      title: 'Nota antes de actualizar',
+      content: 'Contenido antes de actualizar',
+    });
+
+    const notaActualizada = await service.update(nuevaNota.id, {
+      title: 'Nota actualizada',
+      content: 'Contenido actualizado',
+    });
+
+    expect(notaActualizada).toBeDefined();
+    expect(notaActualizada.title).toEqual('Nota actualizada');
+    expect(notaActualizada.content).toEqual('Contenido actualizado');
+
+    const notasEnBaseDatos = await repository.findOneBy({ id: nuevaNota.id });
+    expect(notasEnBaseDatos).toBeDefined();
+    expect(notasEnBaseDatos.title).toEqual(notaActualizada.title);
+    expect(notasEnBaseDatos.content).toEqual(notaActualizada.content);
+  });
+
+  it('Debería lanzar NotFoundException al no encontrar una nota para modificar', async () => {
+    const notaInexistente = 999;
+    try {
+      await service.update(notaInexistente, {
+        title: 'Nota actualizada',
+        content: 'Contenido actualizado',
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+      expect(error.message).toBe(`La nota con id ${notaInexistente} no existe`);
+    }
+  });
+
+  it('Debería eliminar una nota existente', async () => {
+    await repository.query('DELETE FROM nota;');
+
+    const nuevaNota = await repository.save({
+      title: 'Nota para eliminar',
+      content: 'Contenido para eliminar',
+    });
+
+    try {
+      await service.remove(nuevaNota.id);
+    } catch (error) {
+      expect(error).toBeInstanceOf(HttpException);
+      expect(error.getStatus()).toBe(HttpStatus.OK);
+      expect(error.message).toBe(
+        `La nota con id ${nuevaNota.id} ha sido eliminada`,
+      );
+    }
+
+    const notaEliminada = await repository.findOneBy({ id: nuevaNota.id });
+    expect(notaEliminada).toBeNull();
+  });
+
+  it('Debería lanzar NotFoundException al no encontrar una nota para eliminar', async () => {
+    const notaInexistente = 999;
+    try {
+      await service.remove(notaInexistente);
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+      expect(error.message).toBe(`La nota con id ${notaInexistente} no existe`);
+    }
   });
 });
